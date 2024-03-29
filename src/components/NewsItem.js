d298378381e81b25e2e5095e243f42accbcbfdc8
YASHasvi SHUkla <yashasvigot@gmail.com>
2024-03-29 12:49:59 +0530

    done done done done

---
+++ src/components/NewsItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+
+    render() {
+        let { title, description, imageUrl, newsUrl, author, publishedAt, source } = this.props;
+        return (
+            <div className='my-3'>
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }>
+
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+                    <img src={imageUrl ? imageUrl : "https://media.istockphoto.com/id/1369150014/vector/breaking-news-with-world-map-background-vector.jpg?s=612x612&w=0&k=20&c=9pR2-nDBhb7cOvvZU_VdgkMmPJXrBQ4rB1AkTXxRIKM="} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-body-secondary">By {author ? author : "Unknown"}, {new Date(publishedAt).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
